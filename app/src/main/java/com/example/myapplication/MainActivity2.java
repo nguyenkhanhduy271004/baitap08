e921e6e4e724832a1d18eaf551a7731326598722
Kduy Shop <bogiaoffline@gmail.com>
2024-03-23 15:31:01 +0700

    upload source code

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,21 +1,30 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.bumptech.glide.Glide;
 
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
 
     TextView id,userName,userEmail,gender;
     Button btnLogout;
+    private ImageView imageView;
+    private static final int PICK_IMAGE_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        imageView = findViewById(R.id.imageView);
         if(SharedPrefManager.getInstance(this).isLoggedIn()){
             id = findViewById(R.id.textViewId);
             userName = findViewById(R.id.textViewUsername);
@@ -40,6 +49,21 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     public void onClick(View view){
         if(view.equals(btnLogout)){
             SharedPrefManager.getInstance(getApplicationContext()).logout();
+        }
+    }
+
+    public void chooseImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            Glide.with(this).load(uri).into(imageView);
         }
     }
 }
